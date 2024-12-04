95aa4031d0967a2e733acaf250d65a2c640fca06
Ossif <f_koshkarov@mail.ru>
2024-12-04 15:49:59 +0300

    fixes

--- obj/Debug/net8.0/TestApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6940f9619ff4f61232e06eb9bda8f02a889d0bf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acfbc7636e9fe221c269f9d495b54dcfe45b89bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
