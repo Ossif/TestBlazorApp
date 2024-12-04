0b1d55962d09d1067129e783bffaab19308cef58
Ossif <f_koshkarov@mail.ru>
2024-12-04 17:32:44 +0300

    dockerised

--- obj/Debug/net8.0/TestApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95aa4031d0967a2e733acaf250d65a2c640fca06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78c77babde7bda30e7434e18cbeb8709df5c45ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
